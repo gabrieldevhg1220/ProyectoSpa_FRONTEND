468b7c36f81ccc61fffe2bdfabfc400b3caf5c45
gabrieldevhg1220 <gabrielhg.dev1220@outlook.com>
2025-05-29 17:46:27 -0300

    Añadir link del BACKEND desplegado en Rendel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://backendspa-z818.onrender.com' // Utilizar la variable de entorno API_URL si está disponible
+    apiUrl: 'https://spabackend-0mml.onrender.com' // Utilizar la variable de entorno API_URL si está disponible
 };
